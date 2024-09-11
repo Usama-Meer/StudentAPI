f2655d1b9471a0c092c796ecfac08c23499f51c9
Usama Meer <usamameer256@gmail.com>
2024-09-11 17:16:22 +0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAPI.Model;
+
+namespace StudentAPI.Data
+{
+    /*public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        protected ApplicationDbContext()
+        {
+        }
+    }*/
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
